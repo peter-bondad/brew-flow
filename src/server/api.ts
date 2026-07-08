a9dc43b442e2d7c9661a5d9ac005822edea9385b
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-08 13:38:22 +0800

    feat(api): add admin invitation handling and update auth routes

--- src/server/api.ts
+++ src/server/api.ts
@@ -2,8 +2,12 @@ import { auth } from "@/lib/auth";
 import { env } from "@/lib/env";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { requireAdmin } from "@/server/features/admin/admin.middleware";
+import { createInvitationHandler } from "@/server/features/admin/admin.controller";
+import { acceptInvitationHandler } from "@/server/features/invitations/invitation.controller";
+import { Env } from "./types/hono-types";
 
-const app = new Hono()
+const app = new Hono<Env>()
 
   .use(
     "/api/auth/*", // or replace with "*" to enable cors for all routes
@@ -16,11 +20,15 @@ const app = new Hono()
       credentials: true,
     }),
   )
-  .on(["POST", "GET"], "/auth/*", (c) => {
+  .on(["POST", "GET"], "/api/auth/*", (c) => {
     return auth.handler(c.req.raw);
   })
 
-  .get("/hello", (c) => {
+  .use("/api/admin/*", requireAdmin)
+  .post("/api/admin/invitations", createInvitationHandler)
+  .post("/api/invitations/accept", acceptInvitationHandler)
+
+  .get("/api/hello", (c) => {
     return c.json({
       message: "Hello, World!",
     });
