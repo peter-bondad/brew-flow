d774da07848320890bd753469888758bec426779
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-08 13:57:04 +0800

    feat(permissions): update access control roles and permissions for invitation and user

--- src/lib/permissions.ts
+++ src/lib/permissions.ts
@@ -1,15 +1,17 @@
 import { createAccessControl } from "better-auth/plugins/access";
 
 export const statement = {
-  project: ["create", "read", "update", "delete"],
+  invitation: ["create", "read", "delete"],
+  user: ["create", "read", "update", "delete"],
 } as const;
 
 export const ac = createAccessControl(statement);
 
-export const user = ac.newRole({
-  project: ["read"],
+export const admin = ac.newRole({
+  invitation: ["create", "read", "delete"],
+  user: ["create", "read", "update", "delete"],
 });
 
-export const admin = ac.newRole({
-  project: ["create", "read", "update", "delete"],
+export const user = ac.newRole({
+  user: ["read"],
 });
