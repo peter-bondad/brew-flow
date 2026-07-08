035189543948607fe8bd0c7244d7e099d2b2bc86
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-08 09:16:44 +0800

    feat(api): implement CORS middleware for authentication routes

--- src/server/api.ts
+++ src/server/api.ts
@@ -1,13 +1,31 @@
+import { auth } from "@/lib/auth";
+import { env } from "@/lib/env";
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 
 const app = new Hono()
 
-.basePath("/api");
+  .basePath("/api")
 
-app.get("/hello", (c) => {
-  return c.json({
-    message: "Hello, World!",
+  .use(
+    "/api/auth/*", // or replace with "*" to enable cors for all routes
+    cors({
+      origin: env.CORS_ORIGIN, // replace with your origin
+      allowHeaders: ["Content-Type", "Authorization"],
+      allowMethods: ["POST", "GET", "OPTIONS"],
+      exposeHeaders: ["Content-Length"],
+      maxAge: 600,
+      credentials: true,
+    }),
+  )
+  .on(["POST", "GET"], "/api/auth/*", (c) => {
+    return auth.handler(c.req.raw);
+  })
+
+  .get("/hello", (c) => {
+    return c.json({
+      message: "Hello, World!",
+    });
   });
-});
 
 export default app;
