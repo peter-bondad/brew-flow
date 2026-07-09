f3fd330e70f91c360aea11c9d1fc6c1a41cd811a
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-09 21:33:08 +0800

    feat(email): add email service in container for dependency injection

--- src/server/container/index.ts
+++ src/server/container/index.ts
@@ -1,11 +1,27 @@
+import { env } from "@/lib/env";
+import {
+  DevelopmentEmailService,
+  ResendEmailService,
+} from "../email/email.service";
 import { InvitationRepository } from "../features/invitation/invitation.repository";
 import { InvitationService } from "../features/invitation/invitation.service";
 import db from "../infra/database/client";
+import { resend } from "../email/resend";
+
+// email service
+const emailService =
+  env.NODE_ENV === "development"
+    ? new DevelopmentEmailService()
+    : new ResendEmailService(resend);
 
 const invitationRepository = new InvitationRepository(db);
 
-const invitationService = new InvitationService(invitationRepository);
+const invitationService = new InvitationService(
+  invitationRepository,
+  emailService,
+);
 
 export const container = {
   invitationService,
+  emailService,
 };
