f0164e905b4bd2f17c3d8c5ea135ba0128e8a21a
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-10 11:29:54 +0800

    refactor(invitation): add Env variable generic in invitation route

--- src/server/features/invitation/invitation.routes.ts
+++ src/server/features/invitation/invitation.routes.ts
@@ -1,8 +1,9 @@
 import { Hono } from "hono";
 import { createInvitationController } from "./invitation.controller";
 import { requirePermission } from "@/server/middleware/require-permissions";
+import { Env } from "@/server/hono/hono-types";
 
-const invitationRoutes = new Hono()
+const invitationRoutes = new Hono<Env>()
 
   // User Invitation Endpoint
   .post(
