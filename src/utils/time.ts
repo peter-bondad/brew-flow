24c3cf2222e32bdb0cd5759326b22d6e6bc2bc17
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-10 12:50:21 +0800

    refactor(utils): add null checking

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -10,6 +10,8 @@ export function utcMinutesFromNow(minutes: number): Date {
   return new Date(Date.now() + minutes * 60 * 1000);
 }
 
-export function isExpired(date: Date): boolean {
+export function isExpired(date: Date | null | undefined): boolean {
+  if (!date) return false;
+
   return date.getTime() <= Date.now();
 }
