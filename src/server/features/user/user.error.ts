303b320e4bff0ae0afb25be1bd1624b5cc1ca53a
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-13 16:11:18 +0800

    fix(error): added required application error argument

--- src/server/features/user/user.error.ts
+++ src/server/features/user/user.error.ts
@@ -1,7 +1,8 @@
 import { ApplicationError } from "@/server/shared/application.error";
+import { HttpStatus } from "@/server/shared/http-status-code";
 
 export class UserEmailAlreadyExists extends ApplicationError {
   constructor(message = "Email already exists.") {
-    super(message, "EMAIL_ALREADY_EXISTS");
+    super(message, "EMAIL_ALREADY_EXISTS", HttpStatus.CONFLICT);
   }
 }
