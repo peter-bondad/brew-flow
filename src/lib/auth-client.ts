c3fb080da289749e86cc59b86fa068e11bd7b71e
Peter Bondad <bondadpetermaironne@gmail.com>
2026-07-13 17:06:24 +0800

    refactor(authClient): remove appurl config

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -4,7 +4,6 @@ import { adminClient } from "better-auth/client/plugins";
 import { ac, user, admin as adminPluginClient } from "./permissions";
 
 const authClient = createAuthClient({
-  baseURL: env.NEXT_PUBLIC_APP_URL,
   plugins: [
     adminClient({
       ac,
